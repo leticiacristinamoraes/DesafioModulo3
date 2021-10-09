988a14b6470ccf80c0621c9a9ae21cbce10fb6b3
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 15:27:28 -0300

    Criar método construtor da classe Vendedor

--- src/br/com/zup/Vendedor.java
+++ src/br/com/zup/Vendedor.java
@@ -2,4 +2,12 @@ package br.com.zup;
 
 public class Vendedor extends Pessoa {
     private int registroVendedor;
+
+    public Vendedor() {
+    }
+
+    public Vendedor(String nome, String cpf, String email, int registroVendedor) {
+        super(nome, cpf, email);
+        this.registroVendedor = registroVendedor;
+    }
 }
