e7ce5366fb6d67800f9c1774324db24200cbea87
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 15:37:38 -0300

    Criar atributos da classe Venda

--- src/br/com/zup/Venda.java
+++ src/br/com/zup/Venda.java
@@ -1,4 +1,8 @@
 package br.com.zup;
 
 public class Venda {
+    private Vendedor vendedorResponsavel;
+    private Cliente cliente;
+    private double valorDaVenda;
+    private String dataDeRegistro;
 }
