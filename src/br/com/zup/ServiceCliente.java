54ce92c9deeec8b031d7bd3a00c25be253c3bee2
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 20:22:10 -0300

    Testar método cadastrar cliente

--- src/br/com/zup/ServiceCliente.java
+++ src/br/com/zup/ServiceCliente.java
@@ -7,7 +7,7 @@ public class ServiceCliente {
     private static List<Cliente> listaDeClientes = new ArrayList<>();
 
     public static Cliente cadastrarCliente(String nome, String cpf, String email, double renda) {
-        Cliente novoCliente = new Cliente(nome, cpf, email, renda):
+        Cliente novoCliente = new Cliente(nome, cpf, email, renda);
         listaDeClientes.add(novoCliente);
         return novoCliente;
     }
