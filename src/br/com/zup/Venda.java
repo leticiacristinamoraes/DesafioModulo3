e2f47b7da2b614aab66319e9a6a881610ee44111
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 15:38:57 -0300

    Criar getters e setters da classe Venda

--- src/br/com/zup/Venda.java
+++ src/br/com/zup/Venda.java
@@ -15,4 +15,36 @@ public class Venda {
         this.valorDaVenda = valorDaVenda;
         this.dataDeRegistro = dataDeRegistro;
     }
+
+    public Vendedor getVendedorResponsavel() {
+        return vendedorResponsavel;
+    }
+
+    public void setVendedorResponsavel(Vendedor vendedorResponsavel) {
+        this.vendedorResponsavel = vendedorResponsavel;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getValorDaVenda() {
+        return valorDaVenda;
+    }
+
+    public void setValorDaVenda(double valorDaVenda) {
+        this.valorDaVenda = valorDaVenda;
+    }
+
+    public String getDataDeRegistro() {
+        return dataDeRegistro;
+    }
+
+    public void setDataDeRegistro(String dataDeRegistro) {
+        this.dataDeRegistro = dataDeRegistro;
+    }
 }
