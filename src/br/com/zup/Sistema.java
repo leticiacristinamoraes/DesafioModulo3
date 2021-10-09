8f5e7f7454b249badee7fe5566343f3ae8a83978
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 17:24:10 -0300

    Criar método para receber os dados do usuário na classe Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,10 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private static Scanner receberDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
