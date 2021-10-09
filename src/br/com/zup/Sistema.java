8147903d0c7524e9c28d7fb930da2206904600cd
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 17:34:28 -0300

    Receber os dados para cadasro do cliente na classe Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -7,4 +7,12 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+
+    public static Cliente cadastrarCliente() {
+        String nome = receberDados("Informe o nome do cliente:").nextLine();
+        String cpf = receberDados("Informe o CPF do cliente:").nextLine();
+        String email = receberDados("Informe o email do cliente:").nextLine();
+        double renda = receberDados("Informe a renda do cliente:").nextDouble();
+        return ServiceCliente.cadastrarCliente(nome, cpf, email, renda);
+    }
 }
