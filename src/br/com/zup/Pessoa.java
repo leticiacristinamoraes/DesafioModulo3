9d3786a255431c28d400a85aa6efded3330f24a1
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 15:07:12 -0300

    Criar método construtor da classe Pessoa

--- src/br/com/zup/Pessoa.java
+++ src/br/com/zup/Pessoa.java
@@ -5,4 +5,12 @@ public abstract class Pessoa {
     private String cpf;
     private String email;
 
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
 }
