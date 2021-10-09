4e83a12e6c7c5406d3638d2a85bff3931d4cce31
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 20:24:58 -0300

    criar método para receber uma lista de vendedores na classe ServiceVendedor

--- src/br/com/zup/ServiceVenda.java
+++ src/br/com/zup/ServiceVenda.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceVenda {
-    public static List<Venda> vendas = new ArrayList<>();
+    private static List<Venda> vendas = new ArrayList<>();
 
     public static Venda cadastrarVenda(Vendedor vendedorResponsavel, Cliente cliente, double valorDaVenda, String dataDeRegistro) {
         Venda novaVenda = new Venda(vendedorResponsavel, cliente, valorDaVenda, dataDeRegistro);
--- src/br/com/zup/ServiceVendedor.java
+++ src/br/com/zup/ServiceVendedor.java
@@ -1,4 +1,8 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceVendedor {
+    private static List<Vendedor> listaDeVendedores = new ArrayList<>();
 }
