5281f5db29cf0943ad91275ff032f1ab6585832e
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 15:17:59 -0300

    Criar método construtor da classe Cliente

--- src/br/com/zup/Cliente.java
+++ src/br/com/zup/Cliente.java
@@ -2,4 +2,12 @@ package br.com.zup;
 
 public class Cliente extends Pessoa {
     private double renda;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String cpf, String email, double renda) {
+        super(nome, cpf, email);
+        this.renda = renda;
+    }
 }
