df2589a3abdb89e00659b1de2322e9c7e0179a74
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-09 17:09:45 -0300

    Criar método para cadastrar uma nova venda

--- src/br/com/zup/ServiceVenda.java
+++ src/br/com/zup/ServiceVenda.java
@@ -5,4 +5,10 @@ import java.util.List;
 
 public class ServiceVenda {
     public static List<Venda> vendas = new ArrayList<>();
+
+    public static Venda cadastrarVenda(Vendedor vendedorResponsavel, Cliente cliente, double valorDaVenda, String dataDeRegistro) {
+        Venda novaVenda = new Venda(vendedorResponsavel, cliente, valorDaVenda, dataDeRegistro);
+        vendas.add(novaVenda);
+        return novaVenda;
+    }
 }
