063a7e71503ae2280c4748c415b9d0982d1854a3
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-11 12:43:17 -0300

    Criar método para impedir email repetido na classe ServiceVendedor

--- src/br/com/zup/ServiceVendedor.java
+++ src/br/com/zup/ServiceVendedor.java
@@ -8,6 +8,7 @@ public class ServiceVendedor {
 
     public static Vendedor cadastrarVendedor(String nome, String cpf, String email, int registroVendedor) throws Exception {
         validarEmailVendedor(email);
+        impedirEmailRepetidoVendedor(email);
         Vendedor novoVendedor = new Vendedor(nome, cpf, email, registroVendedor);
         listaDeVendedores.add(novoVendedor);
         return novoVendedor;
@@ -31,9 +32,19 @@ public class ServiceVendedor {
         }
     }
 
+    //Método para não permitir cadastro de emails sem @
     public static void validarEmailVendedor(String email) throws Exception {
         if (!email.contains("@")) {
             throw new Exception("Não foi possível cadastrar o vendedor. Por favor, digite um email válido!");
+        }
+    }
+
+    //Método para impedir cadastro de vendedores com email repetido
+    public static void impedirEmailRepetidoVendedor(String email) throws Exception {
+        for (Vendedor vendedorReferencia: listaDeVendedores) {
+            if (vendedorReferencia.getEmail().equals(email)) {
+                throw  new Exception("O email informado já está cadastrado! Por favor, digite um novo email!");
+            }
         }
     }
 }
