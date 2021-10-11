619c488b113ae61cf27d5fa36db6b0d2836c7b20
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-11 12:51:16 -0300

    Criar método para impedir cpf repetido na classe ServiceCliente

--- src/br/com/zup/ServiceCliente.java
+++ src/br/com/zup/ServiceCliente.java
@@ -7,6 +7,7 @@ public class ServiceCliente {
     private static List<Cliente> listaDeClientes = new ArrayList<>();
 
     public static Cliente cadastrarCliente(String nome, String cpf, String email, double renda) throws Exception {
+        impedirCpfRepetidoCliente(cpf);
         validarEmailCliente(email);
         impedirEmailRepetidoCliente(email);
         Cliente novoCliente = new Cliente(nome, cpf, email, renda);
@@ -32,18 +33,27 @@ public class ServiceCliente {
         }
     }
 
-    //Método para não permitir cadastro de emails sem @
+    //Método para não permitir cadastro de clientes com emails sem @
     public static void validarEmailCliente(String email) throws Exception {
         if (!email.contains("@")) {
             throw new Exception("Não foi possível cadastrar o cliente. Por favor, digite um email válido!");
         }
     }
 
-    //Método para impedir cadastros de emails repetidos
+    //Método para impedir cadastro de clientes com emails repetidos
     public static void impedirEmailRepetidoCliente(String email) throws Exception {
         for (Cliente clienteReferencia: listaDeClientes) {
             if (clienteReferencia.getEmail().equals(email)) {
                 throw  new Exception("O email informado já está cadastrado! Por favor, digite um novo email");
+            }
+        }
+    }
+
+    //Método para impedir cadastros de clientes com cpfs repetidos
+    public static void impedirCpfRepetidoCliente(String cpf) throws Exception {
+        for (Cliente clienteReferencia: listaDeClientes) {
+            if (clienteReferencia.getCpf().equals(cpf)) {
+                throw new Exception("O CPF informado já está cadastrado! Por favor, digite um novo CPF!");
             }
         }
     }
